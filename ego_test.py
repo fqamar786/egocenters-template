4db41f81fc786e6948eb6064005da503de04168b
skuzn <skuzn@inbox.ru>
2021-10-06 20:50:03 +0300

    Added test and dataset

---
+++ ego_test.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python3
+
+import egocenters as cc
+import random
+import pytest
+
+centers = [ 0, 107, 1684, 1912, 3437, 348, 3980, 414, 686, 698 ]
+pseudocenters = [ 0, 107, 1684, 1912, 3437, 348, 612, 3980, 414, 686, 698 ]
+
+real_impostor = 612
+
+f = open('facebook_combined.txt')
+lines = f.readlines()
+
+def spl2(x) :
+    y = x.split()
+    return (int(y[0]),int(y[1]))
+
+
+edges = list(map(spl2, lines))
+
+imp = cc.find_impostor(edges, pseudocenters)
+print (imp)
+
+
+
+vertices = list(range(0,4039))
+random.shuffle(vertices)
+print(vertices)
+
+@pytest.mark.parametrize('execution_number', range(6))
+def test_impostor(execution_number):
+    newedges = list(map(lambda uv : (vertices[uv[0]], vertices[uv[1]]), edges))
+    newpseudocenters = list(map(lambda u : vertices[u], pseudocenters))
+    newimp = cc.find_impostor(newedges, newpseudocenters)
+    print (str(newimp) + " " + str(vertices[real_impostor]))
+    return (newimp == vertices[real_impostor])
+    
+
